9be954c846388fe51ba105125a64476866199c1b
CtrlAltDelight <lbchigges@gmail.com>
2023-08-26 01:32:06 -0400

    Just a few more fugitive mappings

--- after/plugin/vim-fugitive.lua
+++ after/plugin/vim-fugitive.lua
@@ -1,1 +1,4 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>ga", ":Git add %<cr>")
+vim.keymap.set("n", "<leader>gc", ":Git commit<cr>")
+vim.keymap.set("n", "<leader>gp", ":Git push<cr>")
