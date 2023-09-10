8180076a39d5fb7c96eb7d5fabd00543ba87f19c
CtrlAltDelight <lbchigges@gmail.com>
2023-09-10 15:54:17 -0400

    Added autopairs for html tags

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,14 +1,37 @@
 return {
-	{ 
-		"nvim-treesitter/nvim-treesitter", 
+	{
+		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		opts = {
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "typescript", "tsx", "javascript", "html", "python", "dockerfile", "java", "latex", "markdown", "json", "css", "rust", "sql", "yaml",  },
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"typescript",
+				"tsx",
+				"javascript",
+				"html",
+				"python",
+				"dockerfile",
+				"java",
+				"latex",
+				"markdown",
+				"json",
+				"css",
+				"rust",
+				"sql",
+				"yaml",
+			},
 			sync_install = false,
 			auto_install = true,
 			highlight = {
 				enable = true,
 				additional_vim_regex_highlighting = false,
+			},
+			autotag = {
+				enable = true,
 			},
 		},
 		config = true,
